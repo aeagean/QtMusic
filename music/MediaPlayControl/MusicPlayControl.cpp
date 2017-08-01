7a56a6cd30439612382a8f87b792587715749f75
Qt君 <2088201923@qq.com>
2017-08-01 09:37:57 +0800

    修改主函数

--- music/MediaPlayControl/MusicPlayControl.cpp
+++ music/MediaPlayControl/MusicPlayControl.cpp
@@ -2,9 +2,9 @@
 
 MusicPlayControl::MusicPlayControl()
 {
-//    m_mediaPlayer->setMedia(QUrl::fromLocalFile("/Users/me/Music/coolsong.mp3"));
-//    m_mediaPlayer->setVolume(50);
-//    m_mediaPlayer->play();
+    m_mediaPlayer.setMedia(QUrl::fromLocalFile("/home/strong/android/otherspace/music/music/resource/我想我不够好.mp3"));
+    m_mediaPlayer.setVolume(50);
+    m_mediaPlayer.play();
 }
 
 bool MusicPlayControl::getIsStart()
