b5714bbac244b10ecf5b5d0c747cc507354be6e7
Qt君 <2088201923@qq.com>
2017-08-09 00:37:40 +0800

    仓库层-增加音乐仓库功能

--- music/Infrastructure/Repertory/MusicRepertory.cpp
+++ music/Infrastructure/Repertory/MusicRepertory.cpp
@@ -12,27 +12,59 @@ MusicRepertory *MusicRepertory::instance()
 
 QList<MusicBase *> MusicRepertory::getMusicBaseList()
 {
-
+    return m_musicBaseList;
 }
 
 MusicBase *MusicRepertory::getMusicBase(QString id)
 {
+    for (int i = 0; i < m_musicBaseList.count(); i++) {
+        if (m_musicBaseList.at(i)->getId() == id) {
+            return m_musicBaseList.at(i);
+        }
+    }
 
+    return NULL;
 }
 
 void MusicRepertory::addMusicBase(MusicBase *musicBase)
 {
+    if (musicBase == NULL)
+        return;
 
+    musicBase->setId(generateRandomId());
+    m_musicBaseList.append(musicBase);
+
+    this->save();
 }
 
 void MusicRepertory::rmMusicBase(QString id)
 {
+    for (int i = 0; i < m_musicBaseList.count(); i++) {
+        if (m_musicBaseList.at(i)->getId() == id) {
+            m_musicBaseList.removeAt(i);
+            break;
+        }
+    }
 
+    this->save();
 }
 
 void MusicRepertory::updateMusicBase(MusicBase *musicBase)
 {
+    if (musicBase == NULL)
+        return;
 
+    for (int i = 0; i < m_musicBaseList.count(); i++) {
+        MusicBase * oldMusicBase = m_musicBaseList.at(i);
+        if (oldMusicBase->getId() == musicBase->getId()) {
+            delete oldMusicBase;
+
+            m_musicBaseList.replace(i, musicBase);
+            break;
+        }
+    }
+
+    this->save();
 }
 
 MusicRepertory::MusicRepertory()
